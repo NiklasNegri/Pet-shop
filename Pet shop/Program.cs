3a02fbe85a4a765a99a970e325c7a05e87f85ccc
Niklas Negri <niklas.negri@gmail.com>
2021-09-23 10:57:06 +0200

    Added answers to assignment questions

--- Pet shop/Program.cs
+++ Pet shop/Program.cs
@@ -8,6 +8,19 @@ namespace Pet_shop
         static void Main(string[] args)
         {
             Menu menu = new Menu();
+
+            // Answering the question:
+            // 1. When should you use private instead of public?
+            // 2. When is it useful to have multiple constructor functions?
+
+            // 1. You should use private when you don't want or need to access the property, class, method etc outside
+            // of the class. Public should only be used when you need access outside of the class itself.
+
+            // 2. Having multiple constructor functions is useful if you need to instance a class with
+            // different values. For example if you need to add a user with admin rights to an administrative program 
+            // you can have a constructor that sets the admin rights to true automatically and then allows
+            // parameters to input the users first and last name. In this case it would be useful to also have a default constructor
+            // that sets the value of admin rights to false.
         }     
     }
 }
